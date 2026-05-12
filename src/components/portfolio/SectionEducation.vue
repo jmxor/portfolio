9449dd25c47a6d1311022a38da9d6292afda5d93
jmxor <jamesmrippeth@gmail.com>
2026-05-12 06:26:02 +0100

    Update education field to be more generic

--- src/components/portfolio/SectionEducation.vue
+++ src/components/portfolio/SectionEducation.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import SectionLabel from "@/components/portfolio/SectionLabel.vue"
 import ArticleLayout from "@/components/portfolio/ArticleLayout.vue"
-import {formatDate} from "@/lib/utils.ts";
+import { formatDate } from "@/lib/utils.ts"
 
 const education = [
   {
     startDate: new Date("2021-09"),
     endDate: new Date("2025-06"),
-    degree: "MSci Computer Science",
+    qualification: "MSci Computer Science",
     institution: "Cardiff University",
     note: [
       "Achieved First Class Honours and Awarded Best Overall Student MSci Computer Science",
@@ -18,14 +18,14 @@ const education = [
   {
     startDate: new Date("2013-09"),
     endDate: new Date("2021-06"),
-    degree: "A Levels",
+    qualification: "A Levels",
     institution: "Aylesbury Grammar School",
     note: ["Computer Science: A, Mathematics: A, Chemistry: A ", "Tutored GCSE chemistry students"],
   },
   {
     startDate: new Date("2013-09"),
     endDate: new Date("2021-06"),
-    degree: "GCSEs",
+    qualification: "GCSEs",
     institution: "Aylesbury Grammar School",
     note: [
       "Physics: 9, Computer Science: 8, Chemistry: 8, Biology: 7, Geography: 7, Mathematics: 7, English Language: 7, English Literature: 6, Business: 6, Statistics: 6",
@@ -41,17 +41,17 @@ const education = [
     <ArticleLayout v-for="(edu, i) in education" :key="i">
       <template #label>
         {{ formatDate(edu.startDate) }} —
-        {{edu.endDate ? formatDate(edu.endDate) : "Present" }}
+        {{ edu.endDate ? formatDate(edu.endDate) : "Present" }}
       </template>
 
       <template #content>
-        <div class="space-y-3">
+        <div class="space-y-1">
           <div class="flex flex-col gap-1 sm:flex-row sm:items-baseline sm:gap-3">
-            <h3 class="text-sm font-semibold text-foreground">{{ edu.degree }}</h3>
+            <h3 class="text-sm font-semibold text-foreground">{{ edu.qualification }}</h3>
             <span class="text-xs text-muted-foreground"> {{ edu.institution }} </span>
           </div>
 
-          <div class="space-y-1">
+          <div class="space-y-3">
             <p
               v-for="(note, j) in edu.note"
               :key="j"
